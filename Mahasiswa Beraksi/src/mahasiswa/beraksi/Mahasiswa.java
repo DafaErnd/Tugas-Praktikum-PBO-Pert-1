d3f72f6dcb1d2dc6f43b21e7e2c0e09447e0628a
DafaErnd <114640090+DaffaErindaZiddana@users.noreply.github.com>
2023-10-11 13:51:44 +0700

    Add files via upload

---
+++ Mahasiswa Beraksi/src/mahasiswa/beraksi/Mahasiswa.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mahasiswa.beraksi;
+
+/**
+ *
+ * @author dafae
+ */
+public class Mahasiswa {
+    void membaca(){
+        System.out.println("Mahasiswa Membaca");
+    }
+    void nyontek(){
+        System.out.println("Mahasiswa Menyontek");
+    }
+    void modifikasi(){
+        System.out.println("Mahasiswa Memodifikasi");
+    }
+}
